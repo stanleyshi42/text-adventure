543fa70e2e45fa6e778c5a80ab18ec763bc68f97
stanleyshi42 <stanleyshi42@gmail.com>
2024-05-24 11:24:44 -0700

    added combine method

--- src/text_adventure/Item.java
+++ src/text_adventure/Item.java
@@ -2,12 +2,15 @@ package text_adventure;
 
 // Various items the player can find in the game
 public enum Item {
-	CANDLE,
-	TORCH,
-	STRING,
-	SPELL_TOME,
-	RED_KEY,
-	BLUE_KEY,
-	YELLOW_KEY
+	TORCH, CANDLE, LIT_CANDLE, SPELL_TOME, RED_KEY, BLUE_KEY, YELLOW_KEY;
 
+	public static Item combine(Inventory inv, Item item1, Item item2) {
+		if (item1 == TORCH && item2 == CANDLE)
+			return LIT_CANDLE;
+		if (item1 == CANDLE && item2 == TORCH)
+			return LIT_CANDLE;
+
+		return null;
+
+	}
 }
