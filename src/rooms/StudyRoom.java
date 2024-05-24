d9d9379ee89b3d31ed1e0272ef0b647fc38df16a
stanleyshi42 <stanleyshi42@gmail.com>
2024-05-24 10:30:25 -0700

    added items and things to interact with

--- src/rooms/StudyRoom.java
+++ src/rooms/StudyRoom.java
@@ -1,10 +1,12 @@
 package rooms;
 
 import text_adventure.ActionParser;
+import text_adventure.Item;
 
 public class StudyRoom extends Room {
 
 	boolean hasYellowKey = true;
+	boolean hasCandle = true;
 
 	@Override
 	public void run() {
@@ -29,7 +31,8 @@ public class StudyRoom extends Room {
 
 	public void printScenario1() {
 		String text = """
-				As you are about to enter the wizard's private study, its door slams shut and begins speaking
+				Just as you are about to enter the wizard's private study, its door slams shut and begins speaking
+
 				"Halt! If you want to enter this study, you must solve my riddle!"
 				"What has a head and a tail, but no body?"
 				"If you check your pockets, you may have plenty!"
@@ -37,11 +40,11 @@ public class StudyRoom extends Room {
 		System.out.print(text);
 	}
 
-	// TODO
 	public void printScenario2() {
 		String text = """
 				"Correct!"
-				The door opens and you enter the study
+				The talking door opens and you enter the study. Despite being a wizard's study, it's quite plain
+				The only things of note are a desk, a chest, and a bookcase
 				""";
 		System.out.print(text);
 	}
@@ -68,10 +71,10 @@ public class StudyRoom extends Room {
 		System.out.print(text);
 	}
 
-	// TODO
 	public void printRoom2() {
 		String text = """
-				print room 2
+				You are in the wizard's private study
+				The only things of note are a desk, a chest, and a bookcase
 				""";
 		System.out.print(text);
 	}
@@ -79,7 +82,7 @@ public class StudyRoom extends Room {
 	public void printEnterFoyer() {
 		String text = """
 				You leave the study and enter the foyer
-				=========================================
+				=======================================
 				""";
 		System.out.print(text);
 	}
@@ -110,6 +113,42 @@ public class StudyRoom extends Room {
 				break;
 			}
 
+			break;
+
+		case "check desk":
+			if (this.hasYellowKey) {
+				player.inventory.add(Item.YELLOW_KEY);
+				this.hasYellowKey = false;
+				System.out.println("You rummage through the desk and find a yellow key!");
+				System.out.println("You take the key and put it in your backpack");
+			} else {
+				System.out.println("You rummage through the desk, but find nothing of value");
+			}
+
+			break;
+
+		case "open chest":
+		case "check chest":
+			if (this.hasCandle) {
+				player.inventory.add(Item.CANDLE);
+				this.hasCandle = false;
+				System.out.println("You open the chest, which contains a candle!");
+				System.out.println("You put the candle in your backpack");
+			} else {
+				System.out.println("You check the chest again, but it's empty");
+			}
+			break;
+
+		case "check bookshelf":
+		case "check bookcase":
+			String text = """
+					You approach the bookcase and reach for a random book
+					The moment your finger touches it, a teleportation spell is cast on you!
+					The next thing you know, you find yourself back in the dungeon
+					==============================================================
+					""";
+			System.out.print(text);
+			this.nextRoom = new DungeonRoom();
 			break;
 
 		case "leave":
