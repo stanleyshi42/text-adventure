a71fc09696f3a43a2c2254ca448f5d82ad80c4d7
stanleyshi42 <stanleyshi42@gmail.com>
2024-05-24 10:40:56 -0700

    added comments

--- src/rooms/Room.java
+++ src/rooms/Room.java
@@ -4,10 +4,12 @@ import text_adventure.Player;
 
 public abstract class Room {
 	public Player player;
+	// Tracks the state of the room. When the player performs certain actions, the
+	// room will increment to its next state
 	int state = 0;
 	public Room nextRoom;
 
-	public abstract void run(); // Called when first entering a room
+	public abstract void run(); // Called when first entering the room
 
 	public abstract void printRoom(); // Prints descriptive text of the room
 
