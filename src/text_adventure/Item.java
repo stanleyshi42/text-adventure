372170a601650097c736bf59a487822b79bf7792
stanleyshi42 <stanleyshi42@gmail.com>
2024-05-24 11:45:20 -0700

    updated combine logic

--- src/text_adventure/Item.java
+++ src/text_adventure/Item.java
@@ -4,13 +4,42 @@ package text_adventure;
 public enum Item {
 	TORCH, CANDLE, LIT_CANDLE, SPELL_TOME, RED_KEY, BLUE_KEY, YELLOW_KEY;
 
-	public static Item combine(Inventory inv, Item item1, Item item2) {
-		if (item1 == TORCH && item2 == CANDLE)
-			return LIT_CANDLE;
-		if (item1 == CANDLE && item2 == TORCH)
-			return LIT_CANDLE;
+	// Given two items, try to combine them
+	public static Item combine(Player player, String item1, String item2) {
+		item1 = item1.trim().toLowerCase();
+		item2 = item2.trim().toLowerCase();
+
+		switch (item1) {
+		case "torch":
+			switch (item2) {
+			case "candle":
+				if (player.inventory.has(TORCH) && player.inventory.has(CANDLE)) {
+					player.getInventory().add(LIT_CANDLE);
+					player.getInventory().remove(CANDLE);
+					return LIT_CANDLE;
+				}
+				break;
+
+			default:
+				break;
+			}
+		case "candle":
+			switch (item2) {
+			case "torch":
+				if (player.inventory.has(TORCH) && player.inventory.has(CANDLE)) {
+					player.getInventory().add(LIT_CANDLE);
+					player.getInventory().remove(CANDLE);
+					return LIT_CANDLE;
+				}
+				break;
+
+			default:
+				break;
+			}
+		default:
+			break;
+		}
 
 		return null;
-
 	}
 }
