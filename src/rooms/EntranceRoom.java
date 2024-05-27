c2dd04e72115606075fd8761933e651516cb84c5
stanleyshi42 <stanleyshi42@gmail.com>
2024-05-27 15:23:56 -0700

    finished entrance

--- src/rooms/EntranceRoom.java
+++ src/rooms/EntranceRoom.java
@@ -1,6 +1,7 @@
 package rooms;
 
 import text_adventure.ActionParser;
+import text_adventure.Item;
 
 public class EntranceRoom extends Room {
 
@@ -17,6 +18,9 @@ public class EntranceRoom extends Room {
 		case (1):
 			printScenario1();
 			break;
+		case (2):
+			printScenario2();
+			break;
 
 		}
 	}
@@ -29,8 +33,16 @@ public class EntranceRoom extends Room {
 		System.out.print(text);
 	}
 
+	public void printScenario2() {
+		String text = """
+				You open the door with the three keys!
+				""";
+		System.out.print(text);
+	}
+
 	@Override
 	public void printRoom() {
+		System.out.println("asdfadfsadfsdafsadfsadfsdafsadfsdaf");
 		switch (state) {
 		case 1:
 			printRoom1();
@@ -81,8 +93,17 @@ public class EntranceRoom extends Room {
 			break;
 
 		case "open door":
-		case "check door":
-			// TODO
+		case "unlock door":
+			if (player.inventory.has(Item.BLUE_KEY) && player.inventory.has(Item.RED_KEY)
+					&& player.inventory.has(Item.YELLOW_KEY)) {
+				nextState();
+				nextRoom = new WinRoom();
+			} else if (player.inventory.has(Item.BLUE_KEY) || player.inventory.has(Item.RED_KEY)
+					|| player.inventory.has(Item.YELLOW_KEY)) {
+				System.out.println("You try unlocking the door, but you don't have enough keys");
+			} else {
+				System.out.println("You try opening the door, but it's locked");
+			}
 			break;
 
 		default:
